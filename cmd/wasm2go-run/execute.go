f31c223d2719e89dc8f3b82b7efdf93542a0be85
lbe <1119377+lbe@users.noreply.github.com>
2026-05-15 19:34:06 -0500

    green(execute): implement execute with IO wiring, exit code propagation, and guaranteed buildDir cleanup Review Performed: Results Approved

--- cmd/wasm2go-run/execute.go
+++ cmd/wasm2go-run/execute.go
@@ -1,7 +1,27 @@
 package main
 
-import "io"
+import (
+	"errors"
+	"io"
+	"os"
+	"os/exec"
+)
 
 func execute(binaryPath, buildDir string, stdout, stderr io.Writer) (int, error) {
+	defer os.RemoveAll(buildDir)
+
+	cmd := exec.Command(binaryPath)
+	cmd.Stdout = stdout
+	cmd.Stderr = stderr
+
+	err := cmd.Run()
+	if err != nil {
+		var exitErr *exec.ExitError
+		if errors.As(err, &exitErr) {
+			return exitErr.ExitCode(), nil
+		}
+		return -1, err
+	}
+
 	return 0, nil
 }
