e5105b540cdea85943e8bbe0b07489842591f39d
lbe <1119377+lbe@users.noreply.github.com>
2026-05-15 22:52:24 -0500

    green(e2e-assemblyscript): implement main() to wire parseConfig, compile, and execute
    Review Performed: Results Approved

--- cmd/wasm2go-run/main.go
+++ cmd/wasm2go-run/main.go
@@ -1,3 +1,31 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	cfg, err := parseConfig(os.Args[1:], os.Stdout)
+	if err != nil {
+		if err == ErrVersionRequested {
+			os.Exit(0)
+		}
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+
+	buildDir, binaryPath, err := compile(cfg.WasmPath, cfg)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+
+	exitCode, err := execute(binaryPath, buildDir, os.Stdout, os.Stderr)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+	os.Exit(exitCode)
+}
+
